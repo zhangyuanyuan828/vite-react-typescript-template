35a36c93f44302ff1d08c2933dbb33f87f02eb9b
夭夭 <zhangyuanyuan828@outlook.com>
2022-10-30 22:08:58 +0800

    chore: compress

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compress from 'rollup-plugin-gzip'
+import { defineConfig } from 'vite'
+import { brotliCompress } from 'zlib'
+import { promisify } from 'util'
+
+const brotli = promisify(brotliCompress)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  build: {
+    emptyOutDir: true
+  },
+  plugins: [react(), compress(), compress({
+    fileName: '.br',
+    customCompression: (content) => brotli(content)
+  })]
 })
